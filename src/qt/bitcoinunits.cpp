b9ba1717f6a083f05016e4dcd94013a0fc5d1e31
BitAltcoin <bitaltcoin@cloudvault.ch>
2016-03-08 07:43:44 +0100

    Update bitcoinunits.cpp

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -34,9 +34,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("SMC");
-    case mBTC: return QString("mSMC");
-    case uBTC: return QString::fromUtf8("μSMC");
+    case BTC: return QString("XBA");
+    case mBTC: return QString("mXBA");
+    case uBTC: return QString::fromUtf8("μXBA");
     default: return QString("???");
     }
 }
@@ -45,9 +45,9 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("SmallChange");
-    case mBTC: return QString("Milli-SmallChange (1 / 1,000)");
-    case uBTC: return QString("Micro-SmallChange (1 / 1,000,000)");
+    case BTC: return QString("BitAltcoin");
+    case mBTC: return QString("Milli-BitAltcoin (1 / 1,000)");
+    case uBTC: return QString("Micro-BitAltcoin (1 / 1,000,000)");
     default: return QString("???");
     }
 }
