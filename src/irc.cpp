d6c73a131a1d3f697adcff32820673e75e0da0b7
BitAltcoin <bitaltcoin@cloudvault.ch>
2016-03-08 06:39:01 +0100

    Update irc.cpp

--- src/irc.cpp
+++ src/irc.cpp
@@ -294,14 +294,14 @@ void ThreadIRCSeed2(void* parg)
         }
         
         if (fTestNet) {
-            Send(hSocket, "JOIN #smallchangeTEST3\r");
-            Send(hSocket, "WHO #smallchangeTEST3\r");
+            Send(hSocket, "JOIN #bitaltcoinTEST3\r");
+            Send(hSocket, "WHO #bitaltcoinTEST3\r");
         } else {
-            // randomly join #smallchange00-#smallchange99
+            // randomly join #bitaltcoin00-#bitaltcoin99
             int channel_number = GetRandInt(100);
             channel_number = 0; // Litecoin: for now, just use one channel
-            Send(hSocket, strprintf("JOIN #smallchange%02d\r", channel_number).c_str());
-            Send(hSocket, strprintf("WHO #smallchange%02d\r", channel_number).c_str());
+            Send(hSocket, strprintf("JOIN #bitaltcoin%02d\r", channel_number).c_str());
+            Send(hSocket, strprintf("WHO #bitaltcoin%02d\r", channel_number).c_str());
         }
 
         int64 nStart = GetTime();
