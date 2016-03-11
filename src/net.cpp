3d18abdcb2ac0b71b57af985834ad47ec2c1860d
BitAltcoin <bitaltcoin@cloudvault.ch>
2016-03-11 08:57:29 +0100

    Update net.cpp

--- src/net.cpp
+++ src/net.cpp
@@ -1149,9 +1149,9 @@ void MapPort()
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strDNSSeed[][2] = {
-    //{"node.bitaltcoin.online", "107.191.46.5"},
-    //{"node.bitaltco.in", "165.225.130.141"},
-    //{"165.225.130.141:27730", "107.191.46.5:27730"},
+    //{"bitaltcoin.online", "node.bitaltcoin.online"},
+    //{"bitaltco.in", "node.bitaltco.in"},
+    //{"bitaltcoin.space", "node.bitaltcoin.space"},
 };
 
 void ThreadDNSAddressSeed(void* parg)
