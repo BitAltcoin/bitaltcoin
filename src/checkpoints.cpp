5858105f7cb906b2ae659eb7586dc5606a416949
BitAltcoin <bitaltcoin@cloudvault.ch>
2016-03-11 13:28:23 +0100

    Update checkpoints.cpp

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -24,25 +24,25 @@ namespace Checkpoints
     //
     static MapCheckpoints mapCheckpoints =
         boost::assign::map_list_of // TODO: needs to adjusted for checkpoint checks, also see main.cpp
-        (         1, uint256("0x9e9a4664bfd6f56d2136dfb47b93b95f9360107e220b111c4aa8e62d211dfb1f"))
+        (         1, uint256("0x8c006330b854ec9186e089296f9f066def657c8b2536c550153793e1bc2d3db8")),
+        (         2, uint256("0xe2e4ed77e8929e2df99017941ae1bbb3529e8408475dde9a8b85f77acf4d6784")),
+        (         5, uint256("0x92ccf402a68f2728b44443bc2e72f73ef7e4752446de70cb8c833e8d791c8b64")),
+        (        10, uint256("0xf364fa21b600c80dc9caa7efb72d2eee8ddf43d2ab344da726fb8f48d71b30b7"))
         ;
 
-   bool CheckBlock(int nHeight, const uint256& hash)
+    bool CheckBlock(int nHeight, const uint256& hash)
     {
         if (fTestNet) return true; // Testnet has no checkpoints
 
         MapCheckpoints::const_iterator i = mapCheckpoints.find(nHeight);
-        if (i == mapCheckpoints.end()) return NULL;
-        // return hash == i->second;
-      return NULL;
+        if (i == mapCheckpoints.end()) return true;
+        return hash == i->second;
     }
 
     int GetTotalBlocksEstimate()
     {
         if (fTestNet) return 0;
-   
-        // return mapCheckpoints.rbegin()->first;
-      return 0;
+        return mapCheckpoints.rbegin()->first;
     }
 
     CBlockIndex* GetLastCheckpoint(const std::map<uint256, CBlockIndex*>& mapBlockIndex)
@@ -54,8 +54,7 @@ namespace Checkpoints
             const uint256& hash = i.second;
             std::map<uint256, CBlockIndex*>::const_iterator t = mapBlockIndex.find(hash);
             if (t != mapBlockIndex.end())
-                // return t->second;
-            return NULL;
+                return t->second;
         }
         return NULL;
     }
