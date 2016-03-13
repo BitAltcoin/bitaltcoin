8387a1c73415507e94847c1fd11d1e2797ec51ef
BitAltcoin <bitaltcoin@cloudvault.ch>
2016-03-13 19:26:43 +0100

    Update protocol.h

--- src/protocol.h
+++ src/protocol.h
@@ -19,7 +19,7 @@
 extern bool fTestNet;
 static inline unsigned short GetDefaultPort(const bool testnet = fTestNet)
 {
-    return testnet ? 127730 : 27730;
+    return testnet ? 12777 : 2777;
 }
 
 
